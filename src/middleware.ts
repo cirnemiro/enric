97f60a14789ad00d5f7ec4619666c6309245b2c7
enric <cirnemiro@gmail.com>
2023-12-18 13:01:01 +0100

    geo

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,8 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
   console.log("middleware GEO",req.geo);
-  console.log(req.headers.get("cf-ipcountry"));
-  console.log("HEADERS",req.headers);
+  // console.log(req.headers.get("cf-ipcountry"));
+  // console.log("HEADERS",req.headers);
   
   
 }
