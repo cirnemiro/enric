236afef6d0fda2311f13a9d5edc7baceee84ada5
enric <cirnemiro@gmail.com>
2023-12-18 12:14:22 +0100

    geo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AuthProvider, useAuth } from "@/hooks/use-auth";
 import Menu from "@/components/menu";
 import Footer from "@/components/footer";
 import { useEffect } from "react";
+import { NextApiHandler } from "next";
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -16,4 +17,15 @@ export default function App({ Component, pageProps }: AppProps) {
         <Footer/>
     </>
   );
+
+}
+export async function getServerSideProps({req}:any) {
+  const forwarded = req.headers["x-forwarded-for"]
+  const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+  console.log("IP",ip);
+  console.log(req.geo);
+  console.log(req);
+  
+  
+  
 }
