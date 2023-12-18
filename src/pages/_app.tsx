df646e6613b74ec5edd7d59f66fd632079cb3a37
enric <cirnemiro@gmail.com>
2023-12-18 12:08:12 +0100

    geo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-import '../global.scss'
 import { AuthProvider, useAuth } from "@/hooks/use-auth";
 import Menu from "@/components/menu";
 import Footer from "@/components/footer";
